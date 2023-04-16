0be23626fa2fe14fc70fbada078d4f6173b32f3a
zhang-glitch <669638556@qq.com>
2023-04-16 20:55:07 +0800

    [feat] lighthouse测试项目

---
+++ README.md
@@ -0,0 +1,11 @@
+## lighthouse测试案例
+### server.js
+开启资源请求压缩
+### model.js
+使用合适的图片
+### template.html
+删除未使用的js脚本
+### webpack.config.js
+使用生产模式打包，会对代码做一些优化处理，压缩等等。
+### App.js
+删除无用的长任务执行 
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+// 资源压缩
+// const compression = require('compression')
+
+// app.use(compression())
+
+app.use(express.static('build'));
+
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log(`Listening on port ${listener.address().port}`);
+});
